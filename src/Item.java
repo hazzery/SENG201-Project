10836459a0e2e912ebb71be0b8f03023364249f9
Harry Parkes <harrydparkes@protonmail.com>
2023-04-06 14:54:03 +1200

    Made Item Purchasable

--- src/Item.java
+++ src/Item.java
@@ -1,10 +1,28 @@
-public class Item {
+public class Item implements Purchasable {
 
-    Athlete.StatType statType;
-    int improvementAmount;
+    private Athlete.StatType statType;
+    private int improvementAmount;
+    private int contractPrice;
+    private int sellBackPrice;
+    private String description;
 
-    public Item(Athlete.StatType statType, int improvementAmount) {
+    public Item(int contractPrice, int sellBackPrice, String description, Athlete.StatType statType, int improvementAmount) {
+        this.contractPrice = contractPrice;
+        this.sellBackPrice = sellBackPrice;
+        this.description = description;
         this.statType = statType;
         this.improvementAmount = improvementAmount;
+    }
+
+    public int getContractPrice() {
+        return this.contractPrice;
+    }
+
+    public int getSellBackPrice() {
+        return this.sellBackPrice;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 }
