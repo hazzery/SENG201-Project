697a547e59f496292cbe08c7adbaa5e4e98b6d5b
Harry Parkes <harrydparkes@protonmail.com>
2023-04-06 14:36:09 +1200

    Athlete constructor and item getters

--- src/Athlete.java
+++ src/Athlete.java
@@ -4,4 +4,34 @@ public class Athlete {
     private int offence;
     private int defence;
     private int current_health;
+
+    public Athlete(String name, int stamina, int offence, int defence, int current_health) {
+        this.name = name;
+        this.stamina = stamina;
+        this.offence = offence;
+        this.defence = defence;
+        this.current_health = current_health;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getStamina() {
+        return this.stamina;
+    }
+
+    public int getOffence() {
+        return this.offence;
+    }
+
+    public int getDefence() {
+        return this.defence;
+    }
+
+    public int getCurrentHealth() {
+        return this.current_health;
+    }
+
+
 }
