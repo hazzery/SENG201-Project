aa86e7a1c9f7ccca6dbf14446cbfe57ffdeb114e
Harry Parkes <harrydparkes@protonmail.com>
2023-04-06 16:24:26 +1200

    Game set-up 1 Asks user for team name and validates input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
+
 public class Main {
+
+    static Scanner inputScanner = new Scanner(System.in);
+
+    static String getTeamName() {
+        boolean valid = false;
+
+        String teamName = "";
+
+        while (!valid) {
+            System.out.println("Enter team name");
+            teamName = inputScanner.nextLine();  // Read user input
+
+            if (teamName.length() < 3) {
+                System.out.println("Team name must be at least 3 characters long");
+                continue;
+            } else if (teamName.length() > 15) {
+                System.out.println("Team name must be at most 15 characters long");
+                continue;
+            }
+
+            if (teamName.matches("[A-Za-z0-9]+")) {
+                valid = true;
+            } else {
+                System.out.println("Team name must contain only letters and numbers");
+            }
+        }
+
+        return teamName;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world! Its me dalieosplif the legendary programmer!");
+        String teamName = getTeamName();
     }
 }
