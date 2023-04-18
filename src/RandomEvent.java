4705f528b984583a4e31de8f0c477891558dda71
Daniel Smith <dsm123@uclive.ac.nz>
2023-04-18 15:54:45 +1200

    Added RandomEvent Class Skeleton

---
+++ src/RandomEvent.java
@@ -0,0 +1,16 @@
+public class RandomEvent {
+    public int chance;
+
+    public RandomEvent(int chance) {
+        this.chance = chance;
+    }
+
+    public void randomStatIncrease(int chance, Athlete athlete){
+    }
+
+    public void randomNewAthlete(int chance){
+    }
+
+    public void randomQuitAthlete(int chance, Athlete athlete){
+    }
+}
