272a96f0fbd4ef24296acec35e703ac08c9a3edb
Harrison Parkes <hpa101@uclive.ac.nz>
2023-05-11 11:47:56 +1200

    added get all athletes function, and optimised array copying in Team

--- src/Team.java
+++ src/Team.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 /**
  * Class the models a team
@@ -7,7 +8,7 @@ public class Team {
     public static final int MIN_SIZE = 4;
     public static final int MAX_RESERVES = 5;
     private final String name;
-    private final ArrayList<Athlete> athletes;
+    private final ArrayList<Athlete> actives;
     private final ArrayList<Athlete> reserves;
 
     /**
@@ -18,13 +19,9 @@ public class Team {
 	 */
     public Team(String name, ArrayList<Athlete> athletes) {
         this.name = name;
-        this.athletes = new ArrayList<>();
-
-        for (Athlete athlete : athletes) {
-            this.athletes.add(athlete);
-        }
-
-        this.reserves = new ArrayList<>();
+        this.actives = new ArrayList<>(athletes.size());
+        this.actives.addAll(athletes);
+        this.reserves = new ArrayList<>(5);
     }
 
      /**
@@ -42,7 +39,7 @@ public class Team {
 	 * @return The number of athletes in the team
 	 */
     public int numActive() {
-        return this.athletes.size();
+        return this.actives.size();
     }
 
     /**
@@ -54,6 +51,32 @@ public class Team {
         return this.reserves.size();
     }
 
+    public Athlete getActive(int index) {
+        return this.actives.get(index);
+    }
+
+    public Athlete getReserve(int index) {
+        return this.reserves.get(index);
+    }
+
+    public void setActive(Athlete athlete) {
+        this.actives.add(athlete);
+        reserves.remove(athlete);
+    }
+
+    public boolean setReserve(Athlete athlete) throws IllegalStateException {
+        if (this.reserves.size() >= MAX_RESERVES) {
+            throw new IllegalStateException("Cannot have more than " + MAX_RESERVES + " reserves");
+        }
+        this.reserves.add(athlete);
+        actives.remove(athlete);
+        return true;
+    }
+
+    Athlete[] fullTeam() {
+        return Stream.concat(actives.stream(), reserves.stream()).toArray(Athlete[]::new);
+    }
+
     /**
 	 * Determines if the team contains the given athlete
 	 *
@@ -61,34 +84,10 @@ public class Team {
 	 * @return `true` if the team contains the athlete, `false` otherwise
      */
     public boolean contains(Athlete athlete) {
-        for (Athlete a : this.athletes) {
-            if (a.equals(athlete)) {
+        for (Athlete a : this.fullTeam()) {
+            if (a.equals(athlete))
                 return true;
-            }
         }
         return false;
     }
-
-    public Athlete getActive(int index) {
-        return this.athletes.get(index);
-    }
-    public Athlete getReserve(int index) {
-        return this.reserves.get(index);
-    }
-
-    public void setActive(Athlete athlete) {
-        this.athletes.add(athlete);
-        reserves.remove(athlete);
-    }
-
-
-    public boolean setReserve(Athlete athlete) throws IllegalStateException {
-        if (this.reserves.size() >= MAX_RESERVES) {
-            throw new IllegalStateException("Cannot have more than " + MAX_RESERVES + " reserves");
-        }
-        this.reserves.add(athlete);
-        athletes.remove(athlete);
-        return true;
-    }
-
 }
