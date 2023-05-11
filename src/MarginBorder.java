845433645414bf620455c108463e57079ba92bd9
Harrison Parkes <hpa101@uclive.ac.nz>
2023-05-11 14:45:59 +1200

    whitespace fix

--- src/MarginBorder.java
+++ src/MarginBorder.java
@@ -3,7 +3,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 
-public class MarginBorder extends CompoundBorder{
+public class MarginBorder extends CompoundBorder {
     public MarginBorder(int borderWidth, Color borderColour, int marginWidth) {
         super(new LineBorder(borderColour, borderWidth), new EmptyBorder(marginWidth, marginWidth, marginWidth, marginWidth));
     }
