dab76eb766a9b03def67be7f8cee2eae57bb58e6
Harry Parkes <harrydparkes@proton.me>
2023-05-15 20:50:05 +1200

    StadiumScreen extends GameScreenPanel

--- src/StadiumScreen.java
+++ src/StadiumScreen.java
@@ -1,5 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class StadiumScreen extends JPanel {
-    private static final int BORDER_WIDTH = 2;
+public class StadiumScreen extends GameScreenPanel {
+    private final MarginBorder marginBorder = new MarginBorder(0, Color.BLACK, 5);
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        contentPanel.setLayout(new GridLayout(0, 1, 0, 0));
+    }
+
+
+    public StadiumScreen(GameScreen gameScreen) {
+        super(GameScreen.Screen.STADIUM, gameScreen);
+    }
 }
