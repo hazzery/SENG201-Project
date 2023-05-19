09b8f43c7a837872f88db00e7be3094f1df40303
Daniel Smith <dsm123@uclive.ac.nz>
2023-05-19 13:03:58 +1200

    added random event code

--- src/RandomEvent.java
+++ src/RandomEvent.java
@@ -1,43 +1,55 @@
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * A class that represents a Random Event
  */
 public class RandomEvent {
     public int chance;
 
-    /**
-	 * Creates a Random Event
-	 *
-	 * @param chance The chance of an event happening
-	 */
-    public RandomEvent(int chance) {
-        this.chance = chance;
+    public GameManager GameManager;
+    public static Athlete athlete;
+
+    static ArrayList<Athlete> athleteList;
+
+
+    public RandomEvent(ArrayList<Athlete> athleteList) {
+        RandomEvent.athleteList = athleteList;
+        randomEvent();
     }
 
-    /**
-	 * C
-     * @param chance
-     * @param athlete
-	 */
-    public void randomStatIncrease(int chance, Athlete athlete) {
 
+    public static void randomEvent() {
+        int chance = ThreadLocalRandom.current().nextInt(0, 15);
+        if (chance == 3) {
+            randomStatIncrease();
+        } else if (chance == 10) {
+            randomNewAthlete();
+        } else if (chance == 14) {
+            randomQuitAthlete();
+        }
     }
 
-    /**
-	 * C
-     *
-     * @param chance
-	 */
-    public void randomNewAthlete(int chance) {
-
+    public static void randomStatIncrease() {
+        int athleteIndex = ThreadLocalRandom.current().nextInt(0, athleteList.size());
+        int statIndex = ThreadLocalRandom.current().nextInt(0, 3);
+        int statIncrease = ThreadLocalRandom.current().nextInt(1, 5);
+        if (statIndex == 0) {
+            athleteList.get(athleteIndex).stamina += statIncrease;
+        } else if (statIndex == 1) {
+            athleteList.get(athleteIndex).offence += statIncrease;
+        } else if (statIndex == 2) {
+            athleteList.get(athleteIndex).defence += statIncrease;
+        }
+        
     }
 
-    /**
-	 * C
-     *
-     * @param chance
-     * @param athlete
-	 */
-    public void randomQuitAthlete(int chance, Athlete athlete) {
+    public static void randomNewAthlete() {
+        athlete = new Athlete();
+        athleteList.add(athlete);
+    }
 
+    public static void randomQuitAthlete() {
+        athleteList.remove(athlete);
     }
 }
